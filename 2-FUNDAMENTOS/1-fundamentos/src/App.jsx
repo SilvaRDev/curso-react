2e6d6467b7c02137fdd036e68b788753db1454fe
Rafael Silva <104386903+SilvaRDev@users.noreply.github.com>
2025-07-28 11:33:01 -0300

    Aula 01 finalizada

---
+++ 2-FUNDAMENTOS/1-fundamentos/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Fundamentos react</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 2-FUNDAMENTOS/1-fundamentos/src/components/FirstComponent.js
@@ -0,0 +1,9 @@
+const firstComponent = () => {
+  return (
+    <div>
+      <h1>Meu primeiro componente!</h1>
+    </div>
+  )
+}
+
+export default firstComponent
