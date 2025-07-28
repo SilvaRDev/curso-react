6b8083132479cf800fcceb3e63ba8bd72829331c
Rafael Silva <104386903+SilvaRDev@users.noreply.github.com>
2025-07-28 12:12:06 -0300

    Update FirstComponent.jsx

--- 2-FUNDAMENTOS/1-fundamentos/src/components/FirstComponent.jsx
+++ 2-FUNDAMENTOS/1-fundamentos/src/components/FirstComponent.jsx
@@ -1,6 +1,17 @@
+// Arquivo de estilo
+
 const FirstComponent = () => {
+  // Essa função faz isso
+
+  /* 
+
+    multi line 
+
+  */
+
   return (
     <div>
+      {/* Algum comentário */}
       <h1>Meu primeiro componente!</h1>
       <p className="teste">Meu texto</p>
     </div>
