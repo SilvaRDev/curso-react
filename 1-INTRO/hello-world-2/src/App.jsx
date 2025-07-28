af22091f711a806c74034e47b9000bf49e477b4e
Rafael Silva <104386903+SilvaRDev@users.noreply.github.com>
2025-07-28 10:06:32 -0300

    Aula 01 finalizada.

---
+++ 1-INTRO/hello-world-2/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello world 2</h1>
+      <p>Criando app react via Vite</p>
+    </>
+  )
+}
+
+export default App
