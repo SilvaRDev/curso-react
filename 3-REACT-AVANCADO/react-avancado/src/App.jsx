b898725e6f9167909ee3730f93643b3a8f876b69
Rafael Silva <104386903+SilvaRDev@users.noreply.github.com>
2025-07-29 15:12:39 -0300

    Aula sobre imagens em public

--- 3-REACT-AVANCADO/react-avancado/src/App.jsx
+++ 3-REACT-AVANCADO/react-avancado/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
 
 function App() {
-
   return (
     <>
-      <h1>React Avançado</h1>      
+      <h1>React Avançado</h1>
+      {/* IMAGEM EM PÚBLIC */}
+      <div>
+        <img src="/img1.jpg" alt="Paisagem" />
+      </div>
     </>
   )
 }
