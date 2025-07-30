f3eb65d942d30faaf400bc5a7e03f85ed9e15f4c
Rafael Silva <104386903+SilvaRDev@users.noreply.github.com>
2025-07-30 09:09:51 -0300

    Aula sobre previous state

--- 3-REACT-AVANCADO/react-avancado/components/ListRender.jsx
+++ 3-REACT-AVANCADO/react-avancado/components/ListRender.jsx
@@ -3,11 +3,20 @@ import { useState } from 'react'
 const ListRender = () => {
   const [list] = useState(['Matheus', 'Pedro', 'Josias', 'Rafael'])
 
-  const [users] = useState([
+  const [users, setUsers] = useState([
     { id: 1, name: 'Matheus', age: 31 },
-    { id: 7257462854, name: 'João', age: 19 },
-    { id: 37236572, name: 'Rafael', age: 20 },
+    { id: 2, name: 'João', age: 19 },
+    { id: 3, name: 'Rafael', age: 20 },
   ])
+
+  const deleteRandom = () => {
+    const randomNumber = Math.floor(Math.random() * 4)
+
+    setUsers((prevUsers) => {
+      console.log(prevUsers)
+      return prevUsers.filter((user) => randomNumber !== user.id)
+    })
+  }
 
   return (
     <div>
@@ -18,9 +27,12 @@ const ListRender = () => {
       </ul>
       <ul>
         {users.map((user) => (
-          <li key={user.id}>{user.name} - {user.age} anos</li>
+          <li key={user.id}>
+            {user.name} - {user.age} anos
+          </li>
         ))}
       </ul>
+      <button onClick={deleteRandom}>Deletar algum usuário</button>
     </div>
   )
 }
