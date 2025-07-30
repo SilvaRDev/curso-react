c27decf1b3ed6e771617e6a547ff5fc8084e83fa
Rafael Silva <104386903+SilvaRDev@users.noreply.github.com>
2025-07-30 08:46:46 -0300

    aula de renderização de lista com key

--- 3-REACT-AVANCADO/react-avancado/components/ListRender.jsx
+++ 3-REACT-AVANCADO/react-avancado/components/ListRender.jsx
@@ -3,11 +3,22 @@ import { useState } from 'react'
 const ListRender = () => {
   const [list] = useState(['Matheus', 'Pedro', 'Josias', 'Rafael'])
 
+  const [users] = useState([
+    { id: 1, name: 'Matheus', age: 31 },
+    { id: 7257462854, name: 'João', age: 19 },
+    { id: 37236572, name: 'Rafael', age: 20 },
+  ])
+
   return (
     <div>
       <ul>
-        {list.map((item) => (
-          <li>{item}</li>
+        {list.map((item, i) => (
+          <li key={i}>{item}</li>
+        ))}
+      </ul>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name} - {user.age} anos</li>
         ))}
       </ul>
     </div>
