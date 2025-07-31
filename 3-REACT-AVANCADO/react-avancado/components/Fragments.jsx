45a6dea361c47d55918486ca1c259ac63ee9c1a9
Rafael Silva <104386903+SilvaRDev@users.noreply.github.com>
2025-07-31 09:00:24 -0300

    Aula de função em prop finalizada

---
+++ 3-REACT-AVANCADO/react-avancado/components/Fragments.jsx
@@ -0,0 +1,11 @@
+const Fragments = ({ propFragment }) => {
+  return (
+    <>
+      <h2>Primeiro título</h2>
+      <h2>Segundo título</h2>
+      <h4>{propFragment}</h4>
+    </>
+  )
+}
+
+export default Fragments
--- 3-REACT-AVANCADO/react-avancado/src/App.jsx
+++ 3-REACT-AVANCADO/react-avancado/src/App.jsx
@@ -10,10 +10,23 @@ import ListRender from '../components/ListRender'
 import ConditionalRender from '../components/ConditionalRender'
 import ShowUserName from '../components/ShowUserName'
 import CarDetails from '../components/CarDetails'
+import Fragments from '../components/Fragments'
+import Container from '../components/Container'
+import ExecuteFunction from '../components/ExecuteFunction'
 
 function App() {
-  const name = 'Joaquim'
+  /* const name = 'Joaquim' */
   const [userName] = useState('Usuário')
+
+  const cars = [
+    { id: 1, brand: 'Ferrari', color: 'Amarelo', newCar: true, km: 0 },
+    { id: 2, brand: 'KIA', color: 'Branco', newCar: false, km: 82452 },
+    { id: 3, brand: 'Renault', color: 'Azul', newCar: false, km: 89865 },
+  ]
+
+  const showMessage = () => (
+    console.log('Essa é a minha função!')
+  )
 
   return (
     <>
@@ -32,10 +45,36 @@ function App() {
       {/* PROPS */}
       <ShowUserName name={userName} />
       {/* DESTRUCTURING */}
-      <CarDetails brand="Chevrolet" km={20000} color="vermelho" newCar={false} />
+      <CarDetails
+        brand="Chevrolet"
+        km={20000}
+        color="vermelho"
+        newCar={false}
+      />
       {/* REAPROVEITANDO */}
       <CarDetails brand="Ford" color="Azul" km={0} newCar={true} />
       <CarDetails brand="Fiat" color="Branco" km={4500} newCar={false} />
+      {/* LOOP EM ARRAY DE OBJETOS */}
+      {cars.map((car) => (
+        <CarDetails
+          key={car.id}
+          brand={car.brand}
+          color={car.color}
+          km={car.km}
+          newCar={car.newCar}
+        />
+      ))}
+      {/* Fragment */}
+      <Fragments propFragment="teste" />
+      {/* CHILDREN */}
+      <Container myValue="testing">
+        <p>E este é o conteúdo!</p>
+      </Container>
+      <Container myValue="testing 2">
+        <h5>Testando o container</h5>
+      </Container>
+      {/* EXECUTAR FUNÇÃO */}
+      <ExecuteFunction myFunction={showMessage} />
     </>
   )
 }
