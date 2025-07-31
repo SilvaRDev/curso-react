c001dafd6e6adc15343ddc598a68f62a236309b1
Rafael Silva <104386903+SilvaRDev@users.noreply.github.com>
2025-07-31 11:56:33 -0300

    Aula sobre css de component adicionada.

--- 4-REACT-E-CSS/reactcss/src/App.jsx
+++ 4-REACT-E-CSS/reactcss/src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
+import MyComponent from './components/MyComponent'
 
 function App() {
   return (
     <>
+      {/* CSS GLOBAL */}
       <h1>React com CSS</h1>
+      {/* CSS DE COMPONENTE */}
+      <MyComponent />
+      <p>Este parágrafo é do App.jsx</p>
     </>
   )
 }
