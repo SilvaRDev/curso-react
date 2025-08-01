2508dc110eb228e3ebb75b8711a3d50a2255d61f
Rafael Silva <104386903+SilvaRDev@users.noreply.github.com>
2025-08-01 11:46:02 -0300

    Home page finalizada

--- 7-SECRET-WORD/secretword/src/App.jsx
+++ 7-SECRET-WORD/secretword/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 
+import StartScreen from './components/StartScreen'
+
 function App() {
   return (
     <div className="App">
-      <h2>Secret word</h2>
+      <StartScreen />
     </div>
   )
 }
