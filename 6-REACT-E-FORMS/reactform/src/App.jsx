f6fea603347124d4b0a08a7d744b57ebe3ce5242
Rafael Silva <104386903+SilvaRDev@users.noreply.github.com>
2025-08-01 08:49:25 -0300

    aulas 1, 2  3 adicionadas

--- 6-REACT-E-FORMS/reactform/src/App.jsx
+++ 6-REACT-E-FORMS/reactform/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import MyForm from './components/MyForm'
 
 function App() {
-
   return (
     <>
-      
+      <h2>Forms</h2>
+      <MyForm />
     </>
   )
 }
---
+++ 6-REACT-E-FORMS/reactform/src/components/MyForm.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import './MyForm.css'
+
+const MyForm = () => {
+  /* Gerenciamento de dados */
+  
+  const [name, setName] = useState()
+  const [email, setEmail] = useState()
+
+  const handleName = (e) => {
+    setName(e.target.value)
+  }
+
+  return (
+    <div>
+      {/* 1- criação de form */}
+      <form>
+        <div>
+          <label htmlFor="name">Nome:</label>
+          <input type="text" name="name" placeholder='Digite o seu nome' onChange={handleName} />
+          {name && <p>{name}</p>}
+        </div>
+        {/* Label envolvendo input */}
+        <label>
+          <span>E-mail</span>
+          <input type="email" name='email' placeholder='Digite o seu e-mail' />
+        </label>
+        <input type="submit" value="Enviar" />
+      </form>
+    </div>
+  )
+}
+
+export default MyForm
