f809a1565db870017ab3e6a911150bf42a19e405
Rafael Silva <104386903+SilvaRDev@users.noreply.github.com>
2025-08-04 09:48:23 -0300

    Funcionalidade de inserir letra no input adicionada.

--- 7-SECRET-WORD/secretword/src/components/Game.jsx
+++ 7-SECRET-WORD/secretword/src/components/Game.jsx
@@ -1,3 +1,4 @@
+import { useState, useRef } from 'react'
 import './Game.css'
 
 const Game = ({
@@ -10,6 +11,19 @@ const Game = ({
   guesses,
   score,
 }) => {
+  const [letter, setLetter] = useState('')
+  const letterInputRef = useRef(null)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    verifyLetter(letter)
+
+    setLetter('')
+
+    letterInputRef.current.focus()
+  }
+
   return (
     <div className="game">
       <p className="points">
@@ -21,7 +35,7 @@ const Game = ({
       </h3>
       <p>Você ainda tem {guesses} tentativas!</p>
       <div className="wordContainer">
-        {letters.map((letter, i) => 
+        {letters.map((letter, i) =>
           guessedLetters.includes(letter) ? (
             <span key={i} className="letter">
               {letter}
@@ -33,8 +47,16 @@ const Game = ({
       </div>
       <div className="letterContainer">
         <p>Tente adivinhar uma letra da palavra</p>
-        <form>
-          <input type="text" name="letter" maxLength="1" required />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="letter"
+            maxLength="1"
+            required
+            onChange={(e) => setLetter(e.target.value)}
+            value={letter}
+            ref={letterInputRef}
+          />
           <button>Jogar!</button>
         </form>
       </div>
