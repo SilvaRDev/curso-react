0883936e1675a0e2e8de1c4a60b545357096f6d2
Rafael Silva <104386903+SilvaRDev@users.noreply.github.com>
2025-08-04 08:29:22 -0300

    Sistema de geração de categoria e palavra adicionado

--- 7-SECRET-WORD/secretword/src/App.jsx
+++ 7-SECRET-WORD/secretword/src/App.jsx
@@ -22,13 +22,63 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name)
   const [words] = useState(wordsList)
 
-  console.log(words)
+  const [pickedWord, setPickedWord] = useState('')
+  const [pickedCategoty, setPickedCategory] = useState('')
+  const [letters, setLetters] = useState([])
+
+  const pickWordAndCategory = () => {
+    // Pick a random category
+    const categories = Object.keys(words)
+    const category =
+      categories[Math.floor(Math.random() * Object.keys(categories).length)]
+
+    console.log(category)
+
+    // Pick a random word
+    const word =
+      words[category][Math.floor(Math.random() * words[category].length)]
+
+    console.log(word)
+
+    return { word, category }
+  }
+
+  // Starts the secret word game
+  const startGame = () => {
+    // Pick word and pick category]
+    const { word, category } = pickWordAndCategory()
+
+    // Create an array of letters
+    let wordLetters = word.split('')
+    
+    wordLetters = wordLetters.map((l) => l.toLowerCase())
+
+    console.log(word, category)
+    console.log(wordLetters)
+
+    // fill states
+    setPickedCategory(category)
+    setPickedWord(word)
+    setLetters(letters)
+
+    setGameStage(stages[1].name)
+  }
+
+  // Process the letter input
+  const verifyLetter = () => {
+    setGameStage(stages[2].name)
+  }
+
+  // Restarts the game
+  const retry = () => {
+    setGameStage(stages[0].name)
+  }
 
   return (
     <div className="App">
-      {gameStage === 'start' && <StartScreen />}
-      {gameStage === 'game' && <Game />}
-      {gameStage === 'end' && <GameOver />}
+      {gameStage === 'start' && <StartScreen startGame={startGame} />}
+      {gameStage === 'game' && <Game verifyLetter={verifyLetter} />}
+      {gameStage === 'end' && <GameOver retry={retry} />}
     </div>
   )
 }
