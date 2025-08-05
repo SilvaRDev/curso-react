f671c8c30e8b73c7af91d8729b4541b20ed5835f
Rafael Silva <104386903+SilvaRDev@users.noreply.github.com>
2025-08-05 11:58:34 -0300

    Update App.jsx

--- 8-REQ-HTTP/reqhttp/src/App.jsx
+++ 8-REQ-HTTP/reqhttp/src/App.jsx
@@ -1,9 +1,33 @@
 import './App.css'
 
+import { useState, useEffect } from 'react'
+
+const url = 'http://localhost:3000/products'
+
 function App() {
+  const [products, setProducts] = useState([])
+
+  // 1 - Resgatando dados
+  useEffect(() => {
+      async function fetchData() {
+        const res = await fetch(url) // Busca os dados da url (no caso, os objetos presentes na API)
+
+        const data = await res.json()
+
+        setProducts(data)
+      }
+
+      fetchData()
+    }, [])
+
   return (
     <>
       <h1>Lista de Produtos</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name} | R${product.price}</li>
+        ))}
+      </ul>
     </>
   )
 }
