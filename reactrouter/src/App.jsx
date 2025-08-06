1efcae6b29e97a14ee23e11a8cd0989108de8ff4
Rafael Silva <104386903+SilvaRDev@users.noreply.github.com>
2025-08-06 15:28:19 -0300

    Rotas dinâmicas adicionadas

--- reactrouter/src/App.jsx
+++ reactrouter/src/App.jsx
@@ -3,23 +3,26 @@ import './App.css'
 // 1 - Config react router
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+// Components
+import Navbar from './components/Navbar'
+
 // Pages
 import Home from './pages/Home'
 import About from './pages/About'
-
-// Components
-import Navbar from './components/Navbar'
+import Product from './pages/Product'
 
 function App() {
   return (
     <>
-    <h1>React Router</h1>
+      <h1>React Router</h1>
       {/* 2 - Links com React Router */}
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/about' element={<About />}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          {/* 4 - Rota dinâmica */}
+          <Route path="/products/:id" element={<Product />} />
         </Routes>
       </BrowserRouter>
     </>
