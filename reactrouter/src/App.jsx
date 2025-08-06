dbd19b0f998dd0a45ce2d1f576eafd6a138e40dc
Rafael Silva <104386903+SilvaRDev@users.noreply.github.com>
2025-08-06 11:53:48 -0300

    Módulo criado.

---
+++ reactrouter/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+// 1 - Config react router
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// Pages
+import Home from './pages/Home'
+import About from './pages/About'
+
+function App() {
+  return (
+    <>
+      <h1>React Router</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/about' element={<About />}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
