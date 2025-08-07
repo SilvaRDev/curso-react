77cb4cb8a3f3e26769ccae4826c8018be4f7681c
Rafael Silva <104386903+SilvaRDev@users.noreply.github.com>
2025-08-07 10:39:35 -0300

    Update App.jsx

--- reactrouter/src/App.jsx
+++ reactrouter/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 
 // 1 - Config react router
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 // Components
 import Navbar from './components/Navbar'
@@ -33,6 +33,8 @@ function App() {
           <Route path="/products/:id" element={<Product />} />
           {/* 9 - Search */}
           <Route path='/products' element={<Search />} />
+          {/* 10 - Redirect */}
+          <Route path='/company' element={<Navigate to='/about'/>} />
           {/* 7 - No match routes (pg 404) */}
           <Route path='*' element={<NotFound />} />
         </Routes>
