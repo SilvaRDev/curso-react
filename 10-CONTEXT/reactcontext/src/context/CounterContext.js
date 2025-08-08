312683b3b186cead7615a46765e9e5b4e2f76e6d
SilvaRDev <rafaelfsilva605@gmail.com>
2025-08-08 11:45:52 -0300

    adicionando provider

--- 10-CONTEXT/reactcontext/src/context/CounterContext.js
+++
@@ -1,6 +0,0 @@
-// 1 - Criar contexto
-import { createContext, useState } from 'react'
-
-export default CounterConext = createContext()
-
-
--- 10-CONTEXT/reactcontext/src/main.jsx
+++ 10-CONTEXT/reactcontext/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { CounterContextProvider } from './context/CounterContext.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* 2 - Criando Provider */}
+    <CounterContextProvider>
+      <App />
+    </CounterContextProvider>
   </StrictMode>,
 )
