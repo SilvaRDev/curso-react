a250a343034a6a869aee96f666e197668619e763
SilvaRDev <rafaelfsilva605@gmail.com>
2025-08-11 09:44:20 -0300

    Refatorando context ao hook

--- 10-CONTEXT/context/src/pages/About.jsx
+++ 10-CONTEXT/context/src/pages/About.jsx
@@ -1,12 +1,14 @@
-import { useContext } from 'react'
-import { CounterContext } from '../context/CounterContext'
+/* import { useContext } from 'react'
+import { CounterContext } from '../context/CounterContext' */
+import { useCounterContext } from '../hooks/useCounterContext'
 
 const About = () => {
-  const {counter} = useContext(CounterContext)
+  /* const {counter} = useContext(CounterContext) */
+  const { counter } = useCounterContext()
 
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Sobre</h1>
       <p>Valor do contador: {counter}</p>
     </div>
   )
