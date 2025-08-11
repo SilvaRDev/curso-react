bd3b212b1b5e22ad31868bb00ff80e36ab31b4bd
SilvaRDev <rafaelfsilva605@gmail.com>
2025-08-11 11:51:06 -0300

    Create config.js

---
+++ 11-MINIBLOG/miniblog/src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAYjk-ELXYy8aai3qinIhAL1mRa1P4UNoE',
+  authDomain: 'miniblog-6c548.firebaseapp.com',
+  projectId: 'miniblog-6c548',
+  storageBucket: 'miniblog-6c548.firebasestorage.app',
+  messagingSenderId: '1057248189620',
+  appId: '1:1057248189620:web:2c20b9ae44f2453aeccbe1',
+}
+
+const app = initializeApp(firebaseConfig)
+
+const db = getFirestore(app)
+
+export { db }
