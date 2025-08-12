e67dc280bef9bf07632700ed9ccf21967046b0c9
SilvaRDev <rafaelfsilva605@gmail.com>
2025-08-12 08:07:01 -0300

    Rotas adicionadas.

--- 11-MINIBLOG/miniblog/src/App.jsx
+++ 11-MINIBLOG/miniblog/src/App.jsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+
+// Pages
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
+
 function App() {
   return (
     <>
-      <h1>Miniblog</h1>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   )
 }
