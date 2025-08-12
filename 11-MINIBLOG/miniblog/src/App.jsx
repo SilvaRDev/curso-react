5cfc236de98fcd462c36bcf657e8c6cddae72508
SilvaRDev <rafaelfsilva605@gmail.com>
2025-08-12 08:18:46 -0300

    Adicionados Navbar e Footer

--- 11-MINIBLOG/miniblog/src/App.jsx
+++ 11-MINIBLOG/miniblog/src/App.jsx
@@ -1,5 +1,9 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
+// Components
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
 // Pages
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
@@ -8,12 +12,14 @@ function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <div className="container">
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
           </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </>
   )
