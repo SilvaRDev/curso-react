3973f421834c8e9d2453b6436797056e260c5833
SilvaRDev <rafaelfsilva605@gmail.com>
2025-08-13 10:52:29 -0300

    Adicionado front-end da página de login

--- 11-MINIBLOG/miniblog/src/pages/Login/Login.jsx
+++ 11-MINIBLOG/miniblog/src/pages/Login/Login.jsx
@@ -1,9 +1,68 @@
 import styles from './Login.module.css'
 
+import { useAuthentication } from '../../hooks/useAuthentication'
+
+import { useState, useEffect } from 'react'
+
 const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+
+  const { createUser, authError, loading } = useAuthentication()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    setError('')
+
+    const user = {
+      email,
+      password,
+    }
+
+    const res = await createUser(user)
+  }
+
   return (
-    <div>
-      <h2>Login</h2>
+    <div className={styles.login}>
+      <h1>Entrar</h1>
+      <p>Faça o login para poder utilizar o sistema</p>
+      <form onSubmit={handleSubmit}>
+        {error && <p className="error">{error}</p>}
+        <label>
+          <span>E-mail:</span>
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="Insira seu melhor e-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>Senha:</span>
+          <input
+            type="password"
+            name="password"
+            required
+            placeholder="Insira sua senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        {!loading && (
+          <button type="submit" className="btn">
+            Cadastrar
+          </button>
+        )}
+        {loading && (
+          <button className="btn" disabled>
+            Aguarde...
+          </button>
+        )}
+      </form>
     </div>
   )
 }
