28d0fb2d9fcd62c71ac2d5f80f59c80f27523f93
SilvaRDev <rafaelfsilva605@gmail.com>
2025-08-15 10:44:29 -0300

    Update Post.jsx

--- 11-MINIBLOG/miniblog/src/pages/Post/Post.jsx
+++ 11-MINIBLOG/miniblog/src/pages/Post/Post.jsx
@@ -6,11 +6,11 @@ import { useFetchDocument } from '../../hooks/useFetchDocument'
 
 const Post = () => {
   const { id } = useParams()
-  const {document: post, loading} = useFetchDocument('posts', id)
+  const { document: post, loading } = useFetchDocument('posts', id)
 
   return (
     <div>
-      {loading && <p>Carregando post...</p> }
+      {loading && <p>Carregando post...</p>}
       {post && (
         <>
           <h1>{post.title}</h1>
