8e51f15a80f54dd71fb99ec2bd2d8118336aa5c6
SilvaRDev <rafaelfsilva605@gmail.com>
2025-08-18 08:32:01 -0300

    Adicionado estilo da dashboard

--- 11-MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.jsx
+++ 11-MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.jsx
@@ -19,7 +19,7 @@ const Dashboard = () => {
   }
 
   return (
-    <div>
+    <div className={styles.dashboard}>
       <h1>Dashboard</h1>
       <p>Gerencie seus posts</p>
       {posts && posts.length === 0 ? (
@@ -31,13 +31,13 @@ const Dashboard = () => {
         </div>
       ) : (
         <>
-          <div>
+          <div className={styles.post_header}>
             <span>Título</span>
             <span>Ações</span>
           </div>
 
           {posts && posts.map((post) => (
-            <div key={post.id}>
+            <div key={post.id} className={styles.post_row}>
               <p>{post.title}</p>
               <div>
                 <Link to={`/posts/${post.id}`} className="btn btn-outline">
