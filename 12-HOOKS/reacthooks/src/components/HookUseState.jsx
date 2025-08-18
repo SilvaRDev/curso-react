b47c9701a197bbf2f809b8b9c832c63698708037
SilvaRDev <rafaelfsilva605@gmail.com>
2025-08-18 10:43:00 -0300

    Update HookUseState.jsx

--- 12-HOOKS/reacthooks/src/components/HookUseState.jsx
+++ 12-HOOKS/reacthooks/src/components/HookUseState.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import { useState } from 'react'
 
 const HookUseState = () => {
   // 1 - useState
@@ -7,22 +7,32 @@ const HookUseState = () => {
   const [name, setName] = useState('Rafael')
 
   const changeNames = () => {
-
     userName = 'João Souza'
 
     setName('Rafael Silva')
-    
-    console.log(userName)
 
+    console.log(userName)
   }
-  
+
+  // 2 - useState e inputs
+  const [age, setAge] = useState(18)
+
   return (
     <div>
       {/* 1 - useState */}
       <h2>useState</h2>
-        <p>Variável: {userName}</p>
-        <p>useState: {name}</p>
-        <button onClick={changeNames}>Mudar nomes</button>
+      <p>Variável: {userName}</p>
+      <p>useState: {name}</p>
+      <button onClick={changeNames}>Mudar nomes</button>
+      {/* 2 - useState e input */}
+      <form>
+        <input
+          type="text"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+      </form>
+      <p>Você tem {age} anos.</p>
       <hr />
     </div>
   )
