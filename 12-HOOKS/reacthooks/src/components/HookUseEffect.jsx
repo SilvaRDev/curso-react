09e65b9a58aff67308af8531161038ad6e153665
SilvaRDev <rafaelfsilva605@gmail.com>
2025-08-19 08:27:43 -0300

    Update HookUseEffect.jsx

--- 12-HOOKS/reacthooks/src/components/HookUseEffect.jsx
+++ 12-HOOKS/reacthooks/src/components/HookUseEffect.jsx
@@ -17,11 +17,22 @@ const HookUseEffect = () => {
     console.log('Serei executado apenas uma vez.')
   }, [])
 
+  // 3 - Ítem no array de deps.
+  const [anotherNumber, setAnotherNumber] = useState(0)
+
+  useEffect(() => {
+    if (anotherNumber > 0) {
+      console.log('Sou executado apenas quando o anotherNumber muda.')
+    }
+  }, [anotherNumber]) 
+
   return (
     <div>
       <h2>useEffect</h2>
       <p>Número: {number}</p>
       <button onClick={changeSomething}>Executar!</button>
+      <p>Another number: {anotherNumber}</p>
+      <button onClick={() => setAnotherNumber(anotherNumber + 1)}>Mudar anotherNumber</button>
       <hr />
     </div>
   )
