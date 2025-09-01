4af5306d88f27cfbd6856977a0c6a70bdc7a46d6
Rafael Silva <rafaelfsilva605@gmail.com>
2025-09-01 11:47:32 -0300

    slice e service do usuário adicionado.

--- 13-REACTGRAM/frontend/src/slices/authSlice.js
+++ 13-REACTGRAM/frontend/src/slices/authSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import authService from '../services/authService'
 
-const user = JSON.parse(localStorage.getItem('User'))
+const user = JSON.parse(localStorage.getItem('user'))
 
 const initialState = {
   user: user ? user : null,
