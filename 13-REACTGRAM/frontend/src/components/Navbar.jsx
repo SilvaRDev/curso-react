638e090c7e86a77dc6a82c1d1513b56f3ba21c24
Rafael Silva <rafaelfsilva605@gmail.com>
2025-09-01 10:25:32 -0300

    Logout implementado no componente de Navbar.

--- 13-REACTGRAM/frontend/src/components/Navbar.jsx
+++ 13-REACTGRAM/frontend/src/components/Navbar.jsx
@@ -15,9 +15,23 @@ import { useAuth } from '../hooks/useAuth'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
+// Redux
+import { logout, reset } from '../slices/authSlice'
+
 const Navbar = () => {
   const { auth } = useAuth()
   const { user } = useSelector((state) => state.auth)
+
+  const naviagett = useNavigate()
+
+  const dispatch = useDispatch()
+
+  const handleLogout = () => {
+    dispatch(logout())
+    dispatch(reset())
+
+    navigate('/login')
+  }
 
   return (
     <nav id="nav">
@@ -36,18 +50,18 @@ const Navbar = () => {
             </li>
             {user && (
               <li>
-                <NavLink to={`/users/${user._id}`} >
+                <NavLink to={`/users/${user._id}`}>
                   <BsFillCameraFill />
                 </NavLink>
               </li>
             )}
             <li>
-              <NavLink to='/profile' >
+              <NavLink to="/profile">
                 <BsFillPersonFill />
               </NavLink>
             </li>
             <li>
-              <span>Sair</span>
+              <span onClick={handleLogout}>Sair</span>
             </li>
           </>
         ) : (
