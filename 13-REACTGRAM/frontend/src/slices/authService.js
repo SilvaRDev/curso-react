37bad85a3c7c12e8ba53a6caa05fb5167a436075
Rafael Silva <rafaelfsilva605@gmail.com>
2025-09-01 08:37:45 -0300

    Validações adicionadas.

--- 13-REACTGRAM/frontend/src/slices/authService.js
+++ 13-REACTGRAM/frontend/src/slices/authService.js
@@ -5,22 +5,20 @@ const register = async (data) => {
   const config = requestConfig('POST', data)
 
   try {
-    const res = await fetch(
-      api + '/users/register',
-      config.then((res) => res.json)
-    ).catch((err) => err)
+    const res = await fetch(api + '/users/register', config)
+      .then((res) => res.json)
+      .catch((err) => err)
 
-    if(res) {
+    if (res) {
       localStorage.setItem('user', JSON.stringify(res))
     }
-
   } catch (error) {
     console.log(error)
   }
 }
 
 const authService = {
-  register
+  register,
 }
 
 export default authService
--- 13-REACTGRAM/frontend/src/slices/authSlice.js
+++ 13-REACTGRAM/frontend/src/slices/authSlice.js
@@ -29,27 +29,27 @@ export const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    reset: (state) => {
-      state.loading = false 
-      state.error = false 
+    reset: (state) => { 
+      state.loading = false  
+      state.error = false  
       state.success = false
-    }
+    } 
   },
-  extraReducers: (builder) => {
-    builder.addCase(register.pending, (state) => {
+  extraReducers: (builder) => { 
+    builder.addCase(register.pending, (state) => { 
       state.loading = true 
-      state.error = false
-    })
-    .addCase(register.fulfilled, (state, action) => {
+      state.error = false 
+    }) 
+    .addCase(register.fulfilled, (state, action) => { 
       state.loading = false 
-      state.success = true
+      state.success = true 
       state.error = null 
       state.user = action.payload 
     })
-    .addCase(register.rejected, (state, action) => {
-      state.loading = false
-      state.error = action.payload
-      state.user = null
+    .addCase(register.rejected, (state, action) => { 
+      state.loading = false 
+      state.error = action.payload 
+      state.user = null 
     })
   }
 })
